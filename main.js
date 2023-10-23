39bf6c494c9382e8ca9f5a60f8bbf2f8da8f4b7f
laizu akram <laizuakram98@gmail.com>
2023-10-23 17:41:42 +0600

    create progress mokup

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed('#tittle-typing', {
+    strings: ['Web Developer', 'Youtuber', 'Curious Learner', 'Programmer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    smartBackspace: true,
+    loop: true,
+  });
