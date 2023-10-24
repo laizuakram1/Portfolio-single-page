844c38c1d7a38893e25ed40074a40e56ef0d16a7
laizu akram <laizuakram98@gmail.com>
2023-10-24 10:58:05 +0600

    create work section

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed('#tittle-typing', {
-    strings: ['Web Developer', 'Youtuber', 'Curious Learner', 'Programmer'],
+    strings: ['Web Developer', 'Content-creator', 'Curious Learner', 'Programmer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
